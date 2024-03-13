26080567de3c8fb37c197f338fa49bd6cc0f0e5e
Delgorithm <delgorithm@gmail.com>
2024-03-13 18:53:44 +0100

    Feature : HTML and CSS added. JS in the making

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+// Déclaration de la variable BtnToDo pour la colonne "To do"
+const btnToDo = document.querySelector('section:nth-of-type(1) button');
+btnToDo.addEventListener('click', () => {
+    console.log('To do');
+})
+
+// Déclaration de la variable BtnDoing pour la colonne "Doing"
+const btnDoing = document.querySelector('section:nth-of-type(2) button');
+btnDoing.addEventListener('click', () => {
+    console.log('Doing');
+})
+
+// Déclaration de la variable BtnDone pour la colonne "Done"
+const btnDone = document.querySelector('section:nth-of-type(3) button');
+btnDone.addEventListener('click', () => {
+    console.log('Done');
+})
+
