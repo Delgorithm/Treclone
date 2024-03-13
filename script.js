7370aade1ec264d15a054142b36cccd443da0d3f
Delgorithm <delgorithm@gmail.com>
2024-03-13 23:26:01 +0100

    Feaature : Possibility to create new task in 'To do'. Later I'll start the drag and drop

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 // Déclaration de la variable BtnToDo pour la colonne "To do"
 const btnToDo = document.querySelector('section:nth-of-type(1) button');
 btnToDo.addEventListener('click', () => {
-    console.log('To do');
+    const articleToDo = document.querySelector('section:nth-of-type(1)');
+    const newArticle = document.createElement('article');
+    const newTask = prompt("Quel To do dois-je ajouter ? : ");
+    const articleParagraph = document.createElement('p');
+    
+    articleParagraph.style.backgroundColor = "#a6e0a6";
+    articleParagraph.style.padding = "1rem";
+    articleParagraph.style.margin = "1rem";
+
+
+    articleParagraph.innerText = newTask;
+    newArticle.appendChild(articleParagraph);
+    articleToDo.appendChild(newArticle);
 })
 
 // Déclaration de la variable BtnDoing pour la colonne "Doing"
 const btnDoing = document.querySelector('section:nth-of-type(2) button');
 btnDoing.addEventListener('click', () => {
-    console.log('Doing');
+    const articleDoing = document.querySelector('section:nth-of-type(2)');
+    const newDoing = document.createElement('article');
+    articleDoing.appendChild(newDoing);
 })
 
 // Déclaration de la variable BtnDone pour la colonne "Done"
 const btnDone = document.querySelector('section:nth-of-type(3) button');
 btnDone.addEventListener('click', () => {
-    console.log('Done');
+    const articleDoing = document.querySelector('section:nth-of-type(3)');
+    const newDone = document.createElement('article');
+    articleDoing.appendChild(newDone);
 })
 
